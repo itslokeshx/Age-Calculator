8d2e91a4e0bb5fc6dbc1cc94e916288ef7808cac
LOKESH A <131357693+thelokesha@users.noreply.github.com>
2024-08-13 16:23:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,19 @@
+function calculateAge() {
+    var DateofB = new Date(document.getElementById("inputone").value);
+    var DateofC = new Date(document.getElementById("inputtwo").value);
 
- 
-function calculateAge(){
-    var DateofB = document.getElementById("inputone").value;
-    var DateofC = document.getElementById("inputtwo").value;
+    var Year = DateofC.getFullYear() - DateofB.getFullYear();
+    var Month = DateofC.getMonth() - DateofB.getMonth();
+    var Day = DateofC.getDate() - DateofB.getDate();
 
-    var DateofBy = DateofB.slice(0,4);
-    var DateofBy =Number(DateofBy);
-    var DateofBm = DateofB.slice(5,7);
-    var DateofBm =Number(DateofBm);
-    var DateofBd =DateofB.slice(8,10);
-    var DateofBd =Number(DateofBd);
+    if (Day < 0) {
+        Month--;
+        Day += new Date(DateofC.getFullYear(), DateofC.getMonth(), 0).getDate();
+    }
+    if (Month < 0) {
+        Year--;
+        Month += 12;
+    }
 
-    var DateofCy = DateofC.slice(0,4);
-    var DateofCy =  Number(DateofCy);
-    var DateofCm = DateofC.slice(5,7);
-    var DateofCm=  Number(DateofCm);
-    var DateofCd =DateofC.slice(8,10);
-    var DateofCd =Number(DateofCd);
-           
-    var Year = DateofCy - DateofBy;
-    var Month;
-    var Day;
-    if(DateofCm >= DateofBm){
-         Month = DateofCm - DateofBm;
-            
-         document.getElementById("output").innerHTML = "You are " + Year + " Years " + Month + " Months " + Day + " Days old";
-        }
-    else if ( DateofCm < DateofBm){
-        year = year - 1;
-         Month = 12 + (DateofCm - DateofBm);
-        
-         document.getElementById("output").innerHTML = "You are " + Year + " Years " + Month + " Months " + Day + " Days old";}
-    if(DateofCd >= DateofBd){
-        Day = DateofCd - DateofBd;
-       
-        document.getElementById("output").innerHTML = "You are " + Year + " Years " + Month + " Months " + Day + " Days old";}
-    else if ( DateofCd < DateofBd){
-        //Month = Month - 1;
-        Day = 30 + (DateofCd - DateofBd);
-   
-        document.getElementById("output").innerHTML = "You are " + Year + " Years " + Month + " Months " + Day + " Days old";}
-       
-   
+    document.getElementById("output").innerHTML = "You are " + Year + " Years " + Month + " Months " + Day + " Days old";
 }
